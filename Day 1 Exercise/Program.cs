4428dd79c6f03d062bae7a911958ca07a24978d0
Chris Paul <cpaul0013@gmail.com>
2021-09-27 14:05:58 -0400

    Second Commit

--- Day 1 Exercise/Program.cs
+++ Day 1 Exercise/Program.cs
@@ -5,8 +5,19 @@ namespace Day_1_Exercise
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { Console.WriteLine("Hello! Welcome to the Indetity Stealer");
+            Console.Write("Please enter your name: ");
+            string UserName = Console.ReadLine();
+
+            Console.Write("Please enter your name: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your hometown: ");
+            string hometown = Console.ReadLine();
+
+            Console.WriteLine("Please enter your favorite food: ");
+            string food = Console.ReadLine();
+            Console.WriteLine($"Your name is {UserName}. Your age is {age}. Your hometown is {hometown}. Your favorite food is {food}.");
         }
     }
 }
